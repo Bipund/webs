72e93db6eeb81d9fafb5a3107e50bc559ee69399
Bipund <Bipund.pandey.3@gmail.com>
2021-02-17 18:46:28 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react"
+import {w3cwebsocket as W3CWebSocket} from "websocket"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+componentDidMount(){
+  let client = new W3CWebSocket("ws://127.0.0.1:8000")
+
+  client.onopen=(data)=>{
+    console.log("connected",data)
+    client.send()
+  }
+  
+}
+ 
+  render(){
+    return (
+      <div className="App">
+          <span>working</span>
+      </div>
+    );
+  }
+  
 }
 
-export default App;
